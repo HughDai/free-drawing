cd1949131d3c01f214b5a8d511a49fa69b693b34
daixu <daixu@rouchi.com>
2021-10-26 12:20:40 +0800

    mock

--- src/components/Stage.vue
+++ src/components/Stage.vue
@@ -20,7 +20,7 @@ import Canvas from '../shared/canvas'
 // import Timemachine from '../shared/timemachine'
 import { throttle, download, readTextFile } from '../shared/utils'
 import { ElMessage } from 'element-plus'
-const mockData = () => import(/* webpackChunkName: "mockdata" */ '../../mocks/free-drawing_1635136696045.json')
+const mockData = () => import(/* webpackChunkName: "mockdata" */ '../../mocks/free-drawing_1635221844352.json')
 
 export default defineComponent({
   name: 'Stage',
